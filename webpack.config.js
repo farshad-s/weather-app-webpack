0e028d6ec003e96f813be99e96e52f4631b1b7fc
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-04 23:03:08 +0000

    changed public path + added gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: __dirname + "/dist",
     filename: "bundle.js",
-    publicPath: "/",
+    publicPath: "/weather-app-webpack/",
   },
   module: {
     rules: [
