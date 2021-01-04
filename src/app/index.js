def64a4c1f08c4821f906e8e9ab02bc6e13a768f
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-04 22:42:42 +0000

    imported styles

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,8 @@ import { toggleDropdown } from "./toggle";
 import { enterKey } from "./enterKey";
 import { accessApi } from "./accessApi";
 
+import "@styles/main.scss";
+
 const App = () => {
   accessApi();
   toggleDropdown();
