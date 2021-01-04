b0edd7a02a03450c453f4f0ef2aad8bf2213634e
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-04 23:34:24 +0000

    changed name for public path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: __dirname + "/dist",
     filename: "bundle.js",
-    publicPath: "/weather-app-webpack/",
+    publicPath: "weather-app-webpack",
   },
   module: {
     rules: [
