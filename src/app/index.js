c26a85a8a8cbbf09a9126def08216949e1d4d6e6
Farshad Shafii <farshad.shafii@gmail.com>
2021-01-04 22:36:44 +0000

    removed code from main index.js + imported it from accessApi

--- src/app/index.js
+++ src/app/index.js
@@ -1,56 +1,9 @@
 import { toggleDropdown } from "./toggle";
 import { enterKey } from "./enterKey";
-
-let dropdownBox = document.getElementById("dropdown-box");
-let dropdownRoot = document.getElementById("dropdown-root");
-let searchbar = document.getElementById("city-search");
-let searchButton = document.getElementById("city-search-button");
-let cityName = "";
-
-let endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},uk&APPID=d8464e2cd289e08057abc0aba345f9b5`;
-
-searchButton.addEventListener("click", function () {
-  dropdownBox.style.display !== "block"
-    ? (dropdownBox.style.display = "block")
-    : (dropdownBox.style.display = "block");
-  cityName = searchbar.value;
-  endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${cityName},uk&APPID=d8464e2cd289e08057abc0aba345f9b5`;
-
-  fetch(endpoint)
-    .then((response) => response.json())
-    .then((data) => {
-      const name = data.name,
-        currentWeather = data.weather[0].main,
-        weatherDescription = data.weather[0].description,
-        minTemp = data.main.temp_min,
-        maxTemp = data.main.temp_max;
-
-      dropdownRoot.innerHTML = `<ul>
-    <li class="dropdown-header">
-      Location <span class="dropdown-info">${name}</span>
-    </li>
-    <li class="dropdown-header">
-      Weather <span class="dropdown-info">${currentWeather}</span>
-    </li>
-    <li class="dropdown-header">
-      Description <span class="dropdown-info">${weatherDescription}</span>
-    </li>
-    <li class="dropdown-header">
-      Minimum Temperature
-      <span class="dropdown-info">${(minTemp - 273.15).toFixed(1)}&#8451</span>
-    </li>
-    <li class="dropdown-header">
-      Maximum Temperature
-      <span class="dropdown-info">${(maxTemp - 273.15).toFixed(1)}&#8451</span>
-    </li>
-  </ul>`;
-    })
-    .catch((error) => {
-      console.log("error is", error);
-    });
-});
+import { accessApi } from "./accessApi";
 
 const App = () => {
+  accessApi();
   toggleDropdown();
   enterKey();
 };
